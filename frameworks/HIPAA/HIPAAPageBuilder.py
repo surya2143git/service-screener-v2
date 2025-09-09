6ad8b42cb71c7b2518297da90e21367ffa41cb78
Suryaprakash V <suryaprakash.v@quadrasystems.net>
2025-09-09 16:04:50 +0530

    Update files

--- frameworks/HIPAA/HIPAAPageBuilder.py
+++ frameworks/HIPAA/HIPAAPageBuilder.py
@@ -3,4 +3,3 @@ from frameworks.FrameworkPageBuilder import FrameworkPageBuilder
 class HIPAAPageBuilder(FrameworkPageBuilder):
     def __init__(self, framework, data):
         super().__init__(framework, data)
-        pass
--- frameworks/PCIDSS/PCIDSSPageBuilder.py
+++ frameworks/PCIDSS/PCIDSSPageBuilder.py
@@ -3,4 +3,3 @@ from frameworks.FrameworkPageBuilder import FrameworkPageBuilder
 class PCIDSSPageBuilder(FrameworkPageBuilder):
     def __init__(self, framework, data):
         super().__init__(framework, data)
-        pass
